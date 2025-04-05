1757598d721ea7a246a2045c2b215d204207751c
J.M.N.R.D. Jayaweera <wazny129@gmail.com>
2025-04-05 23:45:21 +0530

    test classes added

---
+++ VideoRentingSystem/VideoRentingSystem.Tests/Test1.cs
@@ -0,0 +1,11 @@
+﻿namespace VideoRentingSystem.Tests
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
