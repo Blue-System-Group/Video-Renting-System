727438c965eed54e1de119b738c5641527cda0a0
J.M.N.R.D. Jayaweera <119903617+hjhudz@users.noreply.github.com>
2025-04-13 23:48:32 +0530

    input helper class added

---
+++ VideoRentingSystem/VideoRentingSystem/Utils/InputHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoRentingSystem.Utils
+{
+    public class InputHelper
+    {
+    }
+}
